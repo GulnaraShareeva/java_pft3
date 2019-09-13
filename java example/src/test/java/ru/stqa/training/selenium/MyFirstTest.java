652b11512aea888e1b527857e77839495f549ff3
GulnaraShareeva <shareeva.gulnara@yandex.ru>
2019-09-13 18:18:23 +0300

    Update MyFirstTest.java

--- java example/src/test/java/ru/stqa/training/selenium/MyFirstTest.java
+++ java example/src/test/java/ru/stqa/training/selenium/MyFirstTest.java
@@ -6,6 +6,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -19,7 +20,7 @@ public class MyFirstTest {
     @Before
     public void start() {
         driver = new ChromeDriver();
-        wait = new WebDriverWait(driver,80 );
+        wait = new WebDriverWait(driver,200 );
     }
 
 
@@ -29,8 +30,9 @@ public class MyFirstTest {
         driver.findElement(By.name("username")).sendKeys("admin");
         driver.findElement(By.name("password")).sendKeys("admin");
         driver.findElement(By.name("login")).click();
-
-
+        driver.findElement(By.linkText("Countries")).click();
+        driver.findElement(By.linkText("Add New Country")).click();
+        wait.until(titleIs("Add New Country | My Store"));
 
     }
     @After
